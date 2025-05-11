67d20dd10b82b9d98d569cbb47dc2cbf0dfb794d
MidnightX3D <208996522+midnightx3d@users.noreply.github.com>
2025-05-11 23:43:55 +0200

    I dont understand UI

--- mgrsrc/uimgr.py
+++ mgrsrc/uimgr.py
@@ -1,3 +1,27 @@
 """
-PyQt6 UI
+
++-------------------------------+   ← top bar
+|           TOP BAR            |
++-------------+----------------+
+|             |                |
+|   Sidebar   |   Main Page    |   ← middle section
+|             |                |
++-------------+----------------+
+|         BOTTOM BAR           |   ← bottom bar
++-------------------------------+
+
+
 """
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
+
+app = QApplication([])
+
+window = QMainWindow()
+central = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QLabel("Hello World!"))
+central.setLayout(layout)
+window.setCentralWidget(central)
+
+window.show()
+app.exec()
